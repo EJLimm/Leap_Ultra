2572cdee555e8234e892d0a209737450c8858d17
EJLimm <amy020413@hanyang.ac.kr>
2023-05-05 02:44:51 +0900

    try1

---
+++ Assets/ThirdParty/Ultraleap/Tracking/Hands/Runtime/Scripts/PrintMessage.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintMessage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void PrintActivateMessage()
+    {
+        print("Activated");
+    }
+    public void PrintDeactivateMessage()
+    {
+        print("Deactivated");
+    }
+}
